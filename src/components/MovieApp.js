4f3b6752c22b83ee5cc2b35fcec4b5bb39d0e988
Harsh264209 <hsatpute38@gmail.com>
2023-04-17 19:59:10 +0530

    favicon changed

--- src/components/MovieApp.js
+++ src/components/MovieApp.js
@@ -71,58 +71,22 @@ console.log(moviearray)
     return(  
 
       <div className="div">
-       
-      
       <img src={pic1} alt="" />
 
       <div className="main-container">
-
-      <div className="input">
-
-<input type="text" ref={searchInput} placeholder='Search Movie Name' /><button onClick={searchMovie}>Search</button>
-
-
-
-
-</div>
-<div className={loader== true ? 'loader':''}>
-
+        <div className="input">
+        <input type="text" ref={searchInput} placeholder='Search Movie Name' /><button onClick={searchMovie}>Search</button>
+        </div>
+<      div className={loader== true ? 'loader':''}>
       </div>
-      
-     
-     
-      </div>
+  </div>
       
       
-      
-       
-        
-
-       
-
-        
-
-
-
 <div className="div">
-
-
-
-
-{/* 
-<div className="container-sm my-5 px-5">
-  
-
-<div className="input-group mb-3">
-  <input type="text" className="form-control" ref={searchInput}   placeholder="Enter any movie" aria-describedby="button-addon2"/>
-  <button className="btn btn-outline-secondary" onClick={searchMovie}   type="button" id="button-addon2">Search Movie</button>
-</div>
-</div> */}
 
 <div className="container">
 
 {
-
     info.map((movie)=>{ 
 
       if(movie.title != undefined)
@@ -142,13 +106,8 @@ console.log(moviearray)
 }
 
 </div>
-
-
-
- </div>
-
-
-      </div>
+</div>
+</div>
 
 
 
